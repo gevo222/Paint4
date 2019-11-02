56cb70644bd8d90fd168d86031d360f48494b053
gevo222 <51228298+gevo222@users.noreply.github.com>
2019-11-01 23:47:13 -0700

    loading

--- app/src/main/java/com/example/paint4/MainActivity.java
+++ app/src/main/java/com/example/paint4/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.paint4;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.ContentResolver;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +25,16 @@ import org.opencv.core.Mat;
 import org.opencv.core.Scalar;
 import org.opencv.core.Point;
 import org.opencv.imgproc.Imgproc;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import static java.security.AccessController.getContext;
 
 public class MainActivity extends Activity implements View.OnTouchListener {
     static {
@@ -30,10 +50,15 @@ public class MainActivity extends Activity implements View.OnTouchListener {
     Button btp;
     Button btm;
     Button btc;
+    ImageButton bts;
+    ImageButton btl;
     ImageButton btPen;
     ImageButton btEra;
     int thickness;
     int sX, sY, sZ;
+    final int RQS_IMAGE1 = 1;
+    Uri source;
+    Bitmap tempBitmap;
 
 
     @Override
@@ -47,6 +72,8 @@ public class MainActivity extends Activity implements View.OnTouchListener {
         btp = findViewById(R.id.button2);
         btm = findViewById(R.id.button3);
         btc = findViewById(R.id.clearButton);
+        bts = findViewById(R.id.saveButton);
+        btl = findViewById(R.id.loadButton);
         btPen = findViewById(R.id.pencilButton);
         btEra = findViewById(R.id.eraserButton);
 
@@ -93,13 +120,32 @@ public class MainActivity extends Activity implements View.OnTouchListener {
             }
         });
 
+        bts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveBitmapToGallery(bm);
+            }
+        });
 
-
+        btl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, RQS_IMAGE1);
+            }
+        });
         helloworld(2000, 2000);
+
 
     }
 
+    private void saveBitmapToGallery(Bitmap bitmap) {
 
+        String filename = "paint4-" + System.currentTimeMillis() + ".png";
+        String url = MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, filename, "paint4 app");
+
+    }
 
     public void helloworld(int x, int y) {
         // make a mat and draw something
@@ -147,13 +193,63 @@ public class MainActivity extends Activity implements View.OnTouchListener {
 
     }
 
-    public void clear(){
+    public void clear() {
         m = Mat.zeros(1700, 1070, CvType.CV_8UC3);
         bm = Bitmap.createBitmap(m.cols(), m.rows(), Bitmap.Config.ARGB_8888);
         iv.setImageBitmap(bm);
         helloworld(2000, 2000);
     }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case RQS_IMAGE1:
+                    source = data.getData();
+
+                    try {
+                        //tempBitmap is Immutable bitmap,
+                        //cannot be passed to Canvas constructor
+                        tempBitmap = BitmapFactory.decodeStream(
+                                getContentResolver().openInputStream(source));
+
+                        Bitmap.Config config;
+                        if (tempBitmap.getConfig() != null) {
+                            config = tempBitmap.getConfig();
+                        } else {
+                            config = Bitmap.Config.ARGB_8888;
+                        }
+
+                        bm = tempBitmap;
+                        Utils.bitmapToMat(bm,m);
+                        iv.setImageBitmap(bm);
+
+                        //bitmapMaster is Mutable bitmap
+                        /*bitmapMaster = Bitmap.createBitmap(
+                                tempBitmap.getWidth(),
+                                tempBitmap.getHeight(),
+                                config);
+
+                        canvasMaster = new Canvas(bitmapMaster);
+                        canvasMaster.drawBitmap(tempBitmap, 0, 0, null);
+
+                        imageResult.setImageBitmap(bitmapMaster);*/
+                    } catch (FileNotFoundException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+
+                    break;
+            }
+        }
+    }
+
 }
+
 
 
